329ecb1ef9a89a2e47c2a015044509b445541c38
Anurag A <anurag2798@gmail.com>
2019-02-28 23:34:10 +0530

    Error Detection

---
+++ CRC.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+ void main() {
+	int i,j,keylen,msglen,flag;
+	char input[100], key[30],temp[30],quot[100],rem[30],key1[30];
+	printf("Enter the Data : ");
+	scanf("%s",&input);
+	printf("Enter the Key or Divisor: ");
+	scanf("%s",&key);
+	keylen=strlen(key);
+	msglen=strlen(input);
+	strcpy(key1,key);
+	for (i=0;i<keylen-1;i++) {
+		input[msglen+i]='0';
+	}
+	for (i=0;i<keylen;i++)
+	 temp[i]=input[i];
+	for (i=0;i<msglen;i++) {
+		quot[i]=temp[0];
+		if(quot[i]=='0')
+		 for (j=0;j<keylen;j++)
+		 key[j]='0'; else
+		 for (j=0;j<keylen;j++)
+		 key[j]=key1[j];
+		for (j=keylen-1;j>0;j--) {
+			if(temp[j]==key[j])
+			 rem[j-1]='0'; else
+			 rem[j-1]='1';
+		}
+		rem[keylen-1]=input[i+keylen];
+		strcpy(temp,rem);
+	}
+	strcpy(rem,temp);
+	printf("\nQuotient is ");
+	for (i=0;i<msglen;i++)
+	 printf("%c",quot[i]);
+	printf("\nRemainder is ");
+	for (i=0;i<keylen-1;i++)
+	 printf("%c",rem[i]);
+	printf("\nFinal data is: ");
+	for (i=0;i<msglen;i++)
+	 printf("%c",input[i]);
+	for (i=0;i<keylen-1;i++)
+	 printf("%c",rem[i]);
+	 printf("\n");
+	 printf("\n");
+	 printf("Enter the Received Data (excluding the CRC): ");
+	 scanf("%s",&input);
+	keylen=strlen(key);
+	msglen=strlen(input);
+	strcpy(key1,key);
+	for (i=0;i<keylen-1;i++) {
+		input[msglen+i]=rem[i];
+	}
+	printf("The Recieved data is : %s \n",input);
+	for (i=0;i<keylen;i++)
+	 temp[i]=input[i];
+	for (i=0;i<msglen;i++) {
+		quot[i]=temp[0];
+		if(quot[i]=='0')
+		 for (j=0;j<keylen;j++)
+		 key[j]='0'; else
+		 for (j=0;j<keylen;j++)
+		 key[j]=key1[j];
+		for (j=keylen-1;j>0;j--) {
+			if(temp[j]==key[j])
+			 rem[j-1]='0'; else
+			 rem[j-1]='1';
+		}
+		rem[keylen-1]=input[i+keylen];
+		strcpy(temp,rem);
+	}
+	strcpy(rem,temp);
+	printf("\nQuotient is ");
+	for (i=0;i<msglen;i++)
+	 printf("%c",quot[i]);
+	printf("\nRemainder is ");
+	for (i=0;i<keylen-1;i++)
+	 printf("%c",rem[i]);
+	for(i=0;i<keylen-1;i++)
+	{
+        if(rem[i]!='0')
+        {
+        flag=1;
+        }
+        
+}
+        if(flag!=1)
+        {
+        printf("\nThe Data Received is Correct\n");
+        }
+        else
+        {
+        printf("\nThe Data Received is Incorrect\n");
+        }
+	 printf("\n");
+
+}
+
